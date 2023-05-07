ea654b0b5bc2cc0d786f6cc2b98554297efc2060
Daniel Tchernov <tchernovdaniel@gmail.com>
2023-05-07 20:41:44 +0300

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Login from './components/login/Login';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login/>} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
